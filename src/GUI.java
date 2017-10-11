a98d34c19ef2997d9d0397dd6208906cf7a4725c
63628 <hudae_lacximicant.63628@farnborough.ac.uk>
2017-10-11 14:34:11 +0100

    TableView for notes not working yet All the classes have been initialised Notes stage has been completed

--- src/GUI.java
+++ src/GUI.java
@@ -1,10 +1,13 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
@@ -17,6 +20,7 @@ import java.util.Optional;
         private Button modify;
         private Button userSceneButton;
         private Button AssignmentSceneButton;
+        private Button notesButton;
 
         private DatePicker deadline;
         private TextField teacherName;
@@ -25,11 +29,13 @@ import java.util.Optional;
         private TextField title;
         private TextField quantity;
         private TextField format;
- 
+
         private DatePicker DOB;
         private TextField userName;
         private TextField lastName;
 
+        private static Pane notesStage;
+        private TextArea notesTextArea;
 
 
         public static void main(String[] args) {
@@ -45,6 +51,7 @@ import java.util.Optional;
             modify = new Button("Modify");
             userSceneButton = new Button("User");
             AssignmentSceneButton = new Button("Assignments");
+            notesButton = new Button("Notes");
 
             DOB = new DatePicker();
             DOB.setPromptText("Enter DOB");
@@ -69,11 +76,8 @@ import java.util.Optional;
             format.setPromptText("Enter format");
 
 
-
-
-
             HBox assignmentSceneHBox = new HBox();
-            assignmentSceneHBox.getChildren().addAll(add, remove, modify, userSceneButton);
+            assignmentSceneHBox.getChildren().addAll(add, remove, modify, userSceneButton, notesButton);
 
             VBox assignmentSceneVBox = new VBox();
             assignmentSceneVBox.getChildren().addAll(tableView, title, description, className, teacherName, deadline, quantity, format, assignmentSceneHBox);
@@ -92,11 +96,12 @@ import java.util.Optional;
             Scene userScene = new Scene(userSceneVBOX, 300, 150);
             stage.setTitle("School planner");
 
-            add.setOnAction(e -> System.out.println("Adding..."));
-            remove.setOnAction(e -> System.out.println("Removing..."));
-            modify.setOnAction(e -> System.out.println("Modifying..."));
+            add.setOnAction(e -> System.out.println("Add feature has not been implemented"));
+            remove.setOnAction(e -> System.out.println("Remove features has not been implemented"));
+            modify.setOnAction(e -> System.out.println("Modifying feature has not been implemented"));
             userSceneButton.setOnAction(e -> stage.setScene(userScene));
             AssignmentSceneButton.setOnAction(e -> stage.setScene(assignmentScene));
+            notesButton.setOnAction(e -> openNotesStage());
 
             tableView.setMinHeight(470);
 
@@ -121,5 +126,53 @@ import java.util.Optional;
             });
         }
 
+        public void openNotesStage() {
+            Stage stage = new Stage();
+            stage.setTitle("Notes");
+            stage.show();
 
-}
+            VBox notesStageVBox = new VBox();
+            TableView notesTableView = new TableView<>();
+
+
+            ObservableList<String> notesData = FXCollections.observableArrayList();
+            TableColumn notesColumn = new TableColumn<>("Notes");
+
+            notesTableView.setEditable(false);
+            notesTableView.setPrefHeight(500);
+
+
+            HBox notesStageHBox = new HBox();
+            notesStageHBox.setSpacing(10);
+            notesStageHBox.setPadding(new Insets(10, 10,10,10));
+
+
+
+            Button addNote = new Button("Add note");
+            Button deleteNote = new Button("Delete note");
+
+            TextField note = new TextField();
+            note.setPrefWidth(800);
+            notesStageHBox.getChildren().addAll(addNote, deleteNote, note);
+
+
+            notesTableView.getColumns().add(notesColumn);
+
+            addNote.setOnAction(e -> notesData.add(note.getText()));
+
+            notesTableView.setItems(notesData);
+
+
+
+            note.setPromptText("Enter note");
+            notesStageVBox.getChildren().addAll(notesTableView, notesStageHBox);
+
+
+            stage.setScene(new Scene(notesStageVBox));
+
+
+
+
+        }
+
+    }
